ad3c54b55c4a8c2211330b2c33263f8621217865
William Charles <williamcharles@William-Charless-MacBook-Pro.local>
2017-07-27 21:38:44 -0700

    still working

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 3000;
+var jsonParser = bodyParser.json();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
+app.use(bodyParser.json({ type: 'application/*+json'}))
+
+app.use(bodyParser.json({ type: 'application/vnd.custom-type'}))
+
+app.use(bodyParser.text({ type: 'text.html'}))
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+})
