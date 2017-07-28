0f0d62eb944b6a72f3d79a7b0ba5d73c7801732e
William Charles <williamcharles@William-Charless-MacBook-Pro.local>
2017-07-27 22:54:09 -0700

    still working

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(bodyParser.json({ type: 'application/vnd.custom-type'}))
 
 app.use(bodyParser.text({ type: 'text.html'}))
 
+require('./app/routing/htmlRoutes.js')(app);
+
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
 })
